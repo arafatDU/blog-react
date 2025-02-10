41c6fb4d34ac5a278533d96c0c4dc5467bcab777
arafatDU <bsse1435@iit.du.ac.bd>
2025-02-10 19:40:05 +0600

    Setup header and footer

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import store from './store/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/>
+    
+    <App />
     </Provider>
   </React.StrictMode>,
 )
